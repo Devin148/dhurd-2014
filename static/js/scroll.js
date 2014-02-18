357e25e3ca46593eeed4d4229526ab7fccfadffe
Devin148 <devinhurd@gmail.com>
2014-02-18 09:42:24 -0500

    Add navbar links with smooth scrolling

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -34,4 +34,21 @@
 		}
 	});
 
+	// Smooth Scrolling
+	// Good 'ol CSS Tricks http://css-tricks.com/snippets/jquery/smooth-scrolling/
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
 })(jQuery);
